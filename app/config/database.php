43bf2e1731731a0de5cfdd80c5cda466754b5446
Szymon Bluma <szymon.bluma@polcode.net>
2014-01-08 14:47:50 +0100

    minimum service provider on start

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'laravel-blog',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'polcode',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
