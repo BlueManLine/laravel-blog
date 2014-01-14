c026dfdcca75c87c5d602cacbd2126b9588aea8d
Szymon Bluma <szymon.bluma@polcode.net>
2014-01-14 12:49:41 +0100

    templates layouts

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,12 +2,9 @@
 
 class HomeController extends BaseController {
 
-    //protected $layout = 'layouts.master';
-
     public function getIndex()
 	{
         return View::make('hello');
-        //$this->layout->content = 'adfdafad';
 	}
 
 }
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    @section('head')
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,16 +11,15 @@
 
     <!-- Bootstrap core CSS -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
     <link href="/css/template.css" rel="stylesheet">
+    @yield('css_head')
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
-    @show
+    @yield('js_head')
 
 </head>
 
@@ -74,14 +72,13 @@
     <hr>
 
     <footer>
-        <p>&copy; Company 2013</p>
+        <p>&copy; Company 2014</p>
     </footer>
 
 </div><!--/.container-->
 <!-- Placed at the end of the document so the pages load faster -->
-@section('foot')
 <script src="/js-libraries/jquery-1.10.2.min.js"></script>
 <script src="/js-libraries/bootstrap.min.js"></script>
-@show
+@yield('js_foot')
 </body>
 </html>
