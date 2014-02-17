b77401a3562c4824340d3e350e6742191dd8efe3
Szymon Bluma <szymon.bluma@polcode.net>
2014-02-17 14:31:37 +0100

    form post in separate file

--- app/views/admin/posts/create.blade.php
+++ app/views/admin/posts/create.blade.php
@@ -1,38 +1,7 @@
 @extends('layouts.admin.standard')
 
 @section('content')
-<form method="post" role="form">
-    <div class="form-group">
-        <?php echo Form::label('title', 'Title'); ?>
-        <?php echo Form::text('title', null, array('class'=>'form-control','placeholder'=>'Enter post title')); ?>
-        <?php echo \Helpers\Form::errors($errors->first('title')); ?>
-    </div>
-
-    <div class="form-group">
-        <label for="post">Post text</label>
-        <textarea name="post" id="post" cols="10" rows="10" class="form-control"></textarea>
-        <?php echo \Helpers\Form::errors($errors->first('post')); ?>
-    </div>
-
-    <div class="form-group">
-        <label for="optionsRadios1">Visible</label>
-        <div class="radio">
-            <label>
-                <input type="radio" name="status" id="optionsRadios1" value="1" checked>
-                Show
-            </label>
-        </div>
-        <div class="radio">
-            <label>
-                <input type="radio" name="status" id="optionsRadios2" value="0">
-                Hide
-            </label>
-        </div>
-    </div>
-
-    <button type="submit" class="btn btn-default">Submit</button>
-
-</form>
+    @include('admin/posts/form_post', ['post' => null])
 @stop
 
 @section('js_foot')
--- app/views/admin/posts/edit.blade.php
+++ app/views/admin/posts/edit.blade.php
@@ -1,38 +1,7 @@
 @extends('layouts.admin.standard')
 
 @section('content')
-<?php echo Form::model($post, array('class' => 'form-horizontal', 'method' => 'put')); ?>
-    <div class="form-group">
-        <?php echo Form::label('title', 'Title'); ?>
-        <?php echo Form::text('title', null, array('class'=>'form-control','placeholder'=>'Enter post title')); ?>
-        <?php echo \Helpers\Form::errors($errors->first('title')); ?>
-    </div>
-
-    <div class="form-group">
-        <?php echo Form::label('post', 'Post text'); ?>
-        <?php echo Form::textarea('post', null, array('class' => 'form-control', 'rows'=>10, 'cols'=>10)); ?>
-        <?php echo \Helpers\Form::errors($errors->first('post')); ?>
-    </div>
-
-    <div class="form-group">
-        <label for="optionsRadios1">Visible</label>
-        <div class="radio">
-            <label>
-                {{ Form::radio('status', 1) }}
-                Show
-            </label>
-        </div>
-        <div class="radio">
-            <label>
-                {{ Form::radio('status', 0) }}
-                Hide
-            </label>
-        </div>
-    </div>
-
-    <button type="submit" class="btn btn-default">Submit</button>
-
-</form>
+    @include('admin/posts/form_post')
 @stop
 
 @section('js_foot')
