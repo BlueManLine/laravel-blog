965943aafefb05589d094e2d6c4cfc2329cf10e0
Szymon Bluma <szymon.bluma@polcode.net>
2014-02-18 10:44:12 +0100

    removing a link in admin sidebar

--- app/views/admin/partials/sidebar.blade.php
+++ app/views/admin/partials/sidebar.blade.php
@@ -10,7 +10,7 @@
 
 <span class="glyphicon glyphicon-user"></span> Users
 <ul class="nav nav-sidebar">
-    <li><a href="">Add new</a></li>
+    <li><a href="#">Add new</a></li>
     <li class="{{ Helpers\MrView::activeLaravelLink('Admin\UsersController@*Index', 'active') }}"><a href="{{ URL::to('admin/users') }}">List</a></li>
     <li><a href="#">Comments</a></li>
 </ul>
